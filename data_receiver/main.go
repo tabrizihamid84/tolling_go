d9d24db28eba5cee64d41ff59100a92b80e83fd0
tabrizihamid84 <tabrizihamid6367@gmail.com>
2023-09-17 21:40:35 +0330

    first commit

---
+++ data_receiver/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	fmt.Println("receiver working fine!")
+}
+
+// type wsHandler struct {
+// 	websocket.
+// }
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+
+}
